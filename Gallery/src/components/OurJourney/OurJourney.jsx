ad13814c9ee3b50758495d6291fb91670491d89f
Namit Kumar Krrish <199653552+Namitkumarkrrish@users.noreply.github.com>
2026-03-17 03:29:23 +0530

    case sensitivity problem fixed

--- Gallery/src/components/OurJourney/OurJourney.jsx
+++ Gallery/src/components/OurJourney/OurJourney.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { journeyData } from './JourneyData';
+import  journeyData  from './journeyData';
 import { initJourneyAnimations } from './ourJourneyAnimation';
 import styles from './OurJourney.module.css';
 
