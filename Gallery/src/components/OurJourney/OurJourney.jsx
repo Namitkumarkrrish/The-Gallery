a1a65d475b6804227748c2219adf83d244569a6c
Namit Kumar Krrish <199653552+Namitkumarkrrish@users.noreply.github.com>
2026-03-17 03:12:55 +0530

    jouneryData - JourneyData

--- Gallery/src/components/OurJourney/OurJourney.jsx
+++ Gallery/src/components/OurJourney/OurJourney.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { journeyData } from './journeyData';
+import { journeyData } from './JourneyData';
 import { initJourneyAnimations } from './ourJourneyAnimation';
 import styles from './OurJourney.module.css';
 
