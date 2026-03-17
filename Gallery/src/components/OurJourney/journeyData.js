c11c919c7341c5fdd0e0467954b9e8d8e2a2f2a9
Namit Kumar Krrish <199653552+Namitkumarkrrish@users.noreply.github.com>
2026-03-17 20:08:07 +0530

    image place changes

--- Gallery/src/components/OurJourney/journeyData.js
+++ Gallery/src/components/OurJourney/journeyData.js
@@ -18,13 +18,13 @@
  */
 
 import image1 from '../../assets/image1.jpeg';
-import image2 from '../../assets/image2.jpeg';
+import image2 from '../../assets/image7.jpeg';
 import image3 from '../../assets/image3.jpeg';
 import image4 from '../../assets/image4.jpeg';
 import image5 from '../../assets/image5.jpeg';
 import image6 from '../../assets/image6.jpeg';
 import image7 from '../../assets/image8.jpeg';
-import image8 from '../../assets/image7.jpeg';
+import image8 from '../../assets/image2.jpeg';
 
 
 export const journeyData = [
